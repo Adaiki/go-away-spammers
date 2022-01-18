447531afd8b381f472746bc6292cbf97267fb1d4
Adaiki <pro.pereira.nicolas@gmail.com>
2022-01-18 19:20:27 +0100

    Initialize session in init func and create message handler

--- cmd/gas/main.go
+++ cmd/gas/main.go
@@ -10,47 +10,45 @@ import (
 	"github.com/joho/godotenv"
 )
 
-// Version is a constant that stores the Disgord version information.
 const Version = "v0.0.0-alpha"
 
-// Session is declared in the global space so it can be easily used
-// throughout this program.
-// In this use case, there is no error that would be returned.
-var Session, _ = discordgo.New()
+var Session *discordgo.Session
 
 func init() {
 
 	godotenv.Load(".env")
-	// Discord Authentication Token
-	Session.Token = os.Getenv("BOT_TOKEN")
+	Session, _ = discordgo.New("Bot " + os.Getenv("BOT_TOKEN"))
+	Session.SyncEvents = false
 }
 
 func main() {
 
-	// Declare any variables needed later.
-	var err error
-
-	// Verify a Token was provided
 	if Session.Token == "" {
 		log.Println("You must provide a Discord authentication token.")
 		return
 	}
 
-	// Open a websocket connection to Discord
-	err = Session.Open()
+	Session.AddHandler(messageCreate)
+	Session.Identify.Intents = discordgo.IntentsGuildMessages
+
+	err := Session.Open()
 	if err != nil {
 		log.Printf("error opening connection to Discord, %s\n", err)
 		os.Exit(1)
 	}
 
-	// Wait for a CTRL-C
 	log.Printf(`Now running. Press CTRL-C to exit.`)
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
 	<-sc
 
-	// Clean up
 	Session.Close()
+}
 
-	// Exit Normally.
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	// TODO
 }
